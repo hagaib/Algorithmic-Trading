d666aa067c12cf5318c5462b4c0a8890c4d15c4d
Peleg Michaeli <peleg@palgoat.com>
2014-05-01 19:29:20 +0300

    Keep working on train...

--- emas/train.r
+++ emas/train.r
@@ -33,6 +33,7 @@ ema <- function(n)
     dist.cur <- value.cur - mean.cur
 
     ## Calculate the new normalized distance
+    std.cur <- max(std.cur, 0.0001)
     ndist.cur <- dist.cur / std.cur
 
     return(c(value.cur, mean.cur, std.cur, dist.cur, ndist.cur))
@@ -41,16 +42,25 @@ ema <- function(n)
   return(helper)
 }
 
-
-## ----- Train Model ----- ##
-
+## ----- Reading Train Data ----- ##
 ## Read data; we run this only after the prepare data package ran
-data <- readRDS("train.rds")
-print(str(data))
+if(FALSE)
+{
+  print("Reading train data...")
+  data <- readRDS("train.rds")
+} else {
+  print("Reading sample data...")
+  data <- readRDS("sample.rds")
+}
+
+n <- nrow(data)
+print(paste("...", n, "rows"))
+
+
+## ----- Prepare Predictors Matrix ----- ##
+print("Preparing predictors matrix...")
 
 ## We construct a matrix of predictors
-n <- nrow(data)
-
 ## m is the number of predictors for each asset
 m <- 99
 m.all <- m * 5
@@ -75,16 +85,67 @@ for (asset in c('German5', 'German10', 'EUR_USD', 'TYc1', 'US5Y'))
     ema.i <- ema(i)
     ema.i.lst <- Reduce(ema.i, lst, init=rep(NA, 5), accumulate=TRUE)
     ind <- asset.count * m + (i-1)
-    print(paste(asset, ind))
     res <- sapply(ema.i.lst, function(v) v[length(v)])
     predictors[,ind] <- res[2:(n+1)]
   }
 
-  print(paste("Done with", asset))
+  print(paste("... done with predictors of", asset))
   asset.count <- asset.count + 1
 }
 
+## We don't make predictions in the last 2 values
+predictors <- predictors[1:(n-2),]
 
-## TODO: create the response vector
-## TODO: train a random forest
+## We replace NAs with 0s; not optimal, but I can't think of a better solution
+predictors[is.na(predictors)] <- 0
+
+
+## ----- Prepare Response Vectors ----- ##
+print("Preparing response vectors...")
+response <- data.frame(idx=numeric(n-2))
+
+## We actually prepare 5 response vectors, one for each asset.
+## We begin with the naive alternative of the response vector: the sign of the
+## difference between the next-next-open and the next-open.
+for (asset in c('German5', 'German10', 'EUR_USD', 'TYc1', 'US5Y'))
+{
+  asset.o.name <- paste('open', asset, sep='_')
+  asset.o.data <- data[,asset.o.name]
+
+  ## We prepare an asset-specific response vector, and the plug it into our
+  ## response data frame with [,asset].
+  response[,asset] <- sign(diff(asset.o.data)[2:(n-1)])
+
+  ## We replace NAs with 0s; not optimal, but I can't think of a better solution
+  response[,asset][is.na(response[,asset])] <- 0
+
+  print(paste("... done with response of", asset))
+}
+
+## TODO:
+## Later we may consider a more delicate response: instead of taking the simple
+## sign, taking 1 for "significantly higher", -1 for "significantly lower" or 0
+## for "more or less the same", where what defines "significant" is at least
+## 20% of the current bar size (high - low).
+
+
+## ----- Training Random Forest models ----- ##
+print("Training random forest models...")
+require('randomForest')
+
+rf <- data.frame(idx=numeric(n-2))
+for (asset in c('German5', 'German10', 'EUR_USD', 'TYc1', 'US5Y'))
+{
+  resp <- factor(response[,asset])
+  ## We use 'factor' to force classification
+
+  ## We set 'importance=TRUE' to asses importance of predictors
+  rf[,asset] <- randomForest(predictors, resp, importance=TRUE)
+
+  ## TODO: currently we train the model for all values, including NA's replaced
+  ## by 0's. Instead, the model should be trained on data which is not NA in
+  ## the relevant asset (but it can be 0 on the rest).
+
+  print(paste("... done with random forest of", asset))
+}
 
